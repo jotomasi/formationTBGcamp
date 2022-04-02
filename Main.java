7e425b2d36587eb0b40d1f4cbb975f15bf525705
jotomasi <joachim.tomasi.pro@gmail.com>
2022-04-02 14:36:24 +0200

    factorize Arraylist functions into grid and uses these function as object methods in the start

--- Main.java
+++ Main.java
@@ -155,88 +155,6 @@ public class Main extends Application {
 
 
 
-    //Rules function and usefull fonction for Rules
-
-    private boolean neighborhoodAboveThresh(Grid grid, int i, int j, int t) {
-        int cmp = 0;
-        for (int k = 0; k <= 2; k++) {
-            int l = (int) Math.ceil(k / 2.0) % 2;
-            int c = 1 - (int) Math.floor(k / 2.0);
-            if (grid.getcell(i + l, j + c).getLive()) cmp++;
-            if (grid.getcell(i - l, j - c).getLive()) cmp++;
-            if (l == 1 && c == 1 && grid.getcell(i + l, j - c).getLive()) cmp++;
-            if (l == 1 && c == 1 && grid.getcell(i - l, j + c).getLive()) cmp++;
-        }
-        return cmp >= t;
-
-    }
-
-    private boolean neighborhoodBellowThresh(Grid grid, int i, int j, int t) {
-        int cmp = 0;
-        for (int k = 0; k <= 2; k++) {
-            int l = (int) Math.ceil(k / 2.0);
-            int c = 1 - (int) Math.floor(k / 2.0);
-            if (grid.getcell(i + l, j + c).getLive()) cmp++;
-            if (grid.getcell(i - l, j - c).getLive()) cmp++;
-            if (l == 1 && c == 1 && grid.getcell(i + l, j - c).getLive()) cmp++;
-            if (l == 1 && c == 1 && grid.getcell(i - l, j + c).getLive()) cmp++;
-        }
-        return cmp <= t;
-
-    }
-
-    private ArrayList<Integer[]> selectAliveCellIndex(Grid grid) {
-        ArrayList<Integer[]> L = new ArrayList<Integer[]>();
-        for (int i = 0; i < nrow; i++) {
-            for (int j = 0; j < ncol; j++) {
-                if (grid.getcell(i, j).getLive()) L.add(new Integer[]{i, j});
-            }
-        }
-        return L;
-    }
-
-    private ArrayList<Integer[]> selectEmptyNeighborIndex(Grid grid, int i, int j) {
-        ArrayList<Integer[]> Ln = new ArrayList<Integer[]>();
-        for (int k = 0; k <= 2; k++) {
-            int l = (int) Math.ceil(k / 2.0);
-            int c = 1 - (int) Math.floor(k / 2.0);
-            //System.out.println("l="+l+" , c = "+c);
-            if (!grid.getcell(i + l, j + c).getLive()) Ln.add(new Integer[]{i + l, j + c});
-            if (!grid.getcell(i - l, j - c).getLive()) Ln.add(new Integer[]{i - l, j - c});
-            if (l == 1 && c == 1 && !grid.getcell(i + l, j - c).getLive()) Ln.add(new Integer[]{i + l, j - c});
-            if (l == 1 && c == 1 && !grid.getcell(i - l, j + c).getLive()) Ln.add(new Integer[]{i - l, j + c});
-        }
-        return Ln;
-    }
-
-
-    private ArrayList<Integer[]> ListWillBorn(Grid grid, ArrayList<Integer[]> Lalive, int tmin,int tmax) {
-
-        ArrayList<Integer[]> L = new ArrayList<Integer[]>();
-        ArrayList<Integer[]> Ln;//list empty neighborhood
-
-        for (Integer[] index : Lalive) {
-            Ln = new ArrayList<>(selectEmptyNeighborIndex(grid, index[0], index[1]));
-            for (Integer[] neighbor : Ln) {
-                if (neighborhoodAboveThresh(grid, neighbor[0], neighbor[1], tmin)&&neighborhoodBellowThresh(grid, neighbor[0], neighbor[1], tmax)) L.add(neighbor);
-            }
-        }
-        ;
-        return L;
-    }
-
-    private ArrayList<Integer[]> ListWillDead(Grid grid, ArrayList<Integer[]> Lalive, int tmin, int tmax) {
-
-        ArrayList<Integer[]> L = new ArrayList<Integer[]>();
-        ArrayList<Integer[]> Ln;//list empty neighborhood
-
-        for (Integer[] index : Lalive) {
-
-            if (neighborhoodAboveThresh(grid, index[0], index[1], tmax)) L.add(index);
-            if (neighborhoodBellowThresh(grid, index[0], index[1], tmin)) L.add(index);
-        }
-        return L;
-    }
 
     private void updategrid(GraphicsContext gc, Grid grid, ArrayList<Integer[]> Lborn, ArrayList<Integer[]> Ldie) {
         for (Integer[] indexborn : Lborn) updateBornedCell(gc, grid, indexborn[0], indexborn[1]);
@@ -386,9 +304,9 @@ public class Main extends Application {
             public void handle(MouseEvent mouseEvent) {
 
                 if(!isrunning) {
-                    ArrayList<Integer[]> L = new ArrayList<Integer[]>(selectAliveCellIndex(grid));
-                    ArrayList<Integer[]> Lborn = new ArrayList<Integer[]>(ListWillBorn(grid, L, tbirthmin, tbirthmax));
-                    ArrayList<Integer[]> Ldead = new ArrayList<Integer[]>(ListWillDead(grid, L, tdeathLonelyness, tdeathOverpopulated));
+                    ArrayList<Integer[]> L = new ArrayList<Integer[]>(grid.selectAliveCellIndex());
+                    ArrayList<Integer[]> Lborn = new ArrayList<Integer[]>(grid.ListWillBorn( L, tbirthmin, tbirthmax));
+                    ArrayList<Integer[]> Ldead = new ArrayList<Integer[]>(grid.ListWillDead( L, tdeathLonelyness, tdeathOverpopulated));
                     updategrid(gc, grid, Lborn, Ldead);
                 }
             }
@@ -469,9 +387,9 @@ public class Main extends Application {
                     PrevTime = now;
                     //System.out.println("hi");
                     //Update the grid after dt time while the action is running
-                    ArrayList<Integer[]> L = new ArrayList<Integer[]>(selectAliveCellIndex(grid));
-                    ArrayList<Integer[]> Lborn = new ArrayList<Integer[]>(ListWillBorn(grid, L, tbirthmin,tbirthmax));
-                    ArrayList<Integer[]> Ldead = new ArrayList<Integer[]>(ListWillDead(grid, L, tdeathLonelyness, tdeathOverpopulated));
+                    ArrayList<Integer[]> L = new ArrayList<Integer[]>(grid.selectAliveCellIndex());
+                    ArrayList<Integer[]> Lborn = new ArrayList<Integer[]>(grid.ListWillBorn( L, tbirthmin,tbirthmax));
+                    ArrayList<Integer[]> Ldead = new ArrayList<Integer[]>(grid.ListWillDead( L, tdeathLonelyness, tdeathOverpopulated));
                     updategrid(gc, grid, Lborn, Ldead);
 
                 }
--- Project/Grid.java
+++ Project/Grid.java
@@ -1,6 +1,8 @@
 package Project;
 
 
+import java.util.ArrayList;
+
 public class Grid {
     //attribute
     private int ncol = 1; // nombre de colonnes
@@ -27,6 +29,104 @@ public class Grid {
 
     private void setncol(int n){this.ncol = n;}
     private void setnrow(int n){this.nrow = n;}
+
+
+    //Rules function and usefull fonction for Rules
+
+    private boolean neighborhoodAboveThresh( int i, int j, int t) {
+        int cmp = 0;
+        for (int k = 0; k <= 2; k++) {
+            int l = (int) Math.ceil(k / 2.0) % 2;
+            int c = 1 - (int) Math.floor(k / 2.0);
+            if (this.getcell(i + l, j + c).getLive()) cmp++;
+            if (this.getcell(i - l, j - c).getLive()) cmp++;
+            if (l == 1 && c == 1 && this.getcell(i + l, j - c).getLive()) cmp++;
+            if (l == 1 && c == 1 && this.getcell(i - l, j + c).getLive()) cmp++;
+        }
+        return cmp >= t;
+
+    }
+
+    private boolean neighborhoodBellowThresh( int i, int j, int t) {
+        int cmp = 0;
+        for (int k = 0; k <= 2; k++) {
+            int l = (int) Math.ceil(k / 2.0);
+            int c = 1 - (int) Math.floor(k / 2.0);
+            if (this.getcell(i + l, j + c).getLive()) cmp++;
+            if (this.getcell(i - l, j - c).getLive()) cmp++;
+            if (l == 1 && c == 1 && this.getcell(i + l, j - c).getLive()) cmp++;
+            if (l == 1 && c == 1 && this.getcell(i - l, j + c).getLive()) cmp++;
+        }
+        return cmp <= t;
+
+    }
+
+    
+    
+    
+    public ArrayList<Integer[]> selectAliveCellIndex() {
+        ArrayList<Integer[]> L = new ArrayList<Integer[]>();
+        for (int i = 0; i < nrow; i++) {
+            for (int j = 0; j < ncol; j++) {
+                if (this.getcell(i, j).getLive()) L.add(new Integer[]{i, j});
+            }
+        }
+        return L;
+    }
+
+    public ArrayList<Integer[]> selectEmptyNeighborIndex( int i, int j) {
+        ArrayList<Integer[]> Ln = new ArrayList<Integer[]>();
+        for (int k = 0; k <= 2; k++) {
+            int l = (int) Math.ceil(k / 2.0);
+            int c = 1 - (int) Math.floor(k / 2.0);
+            //System.out.println("l="+l+" , c = "+c);
+            if (!this.getcell(i + l, j + c).getLive()) Ln.add(new Integer[]{i + l, j + c});
+            if (!this.getcell(i - l, j - c).getLive()) Ln.add(new Integer[]{i - l, j - c});
+            if (l == 1 && c == 1 && !this.getcell(i + l, j - c).getLive()) Ln.add(new Integer[]{i + l, j - c});
+            if (l == 1 && c == 1 && !this.getcell(i - l, j + c).getLive()) Ln.add(new Integer[]{i - l, j + c});
+        }
+        return Ln;
+    }
+
+
+    public ArrayList<Integer[]> ListWillBorn(ArrayList<Integer[]> Lalive, int tmin,int tmax) {
+
+        ArrayList<Integer[]> L = new ArrayList<Integer[]>();
+        ArrayList<Integer[]> Ln;//list empty neighborhood
+
+        for (Integer[] index : Lalive) {
+            Ln = new ArrayList<>(this.selectEmptyNeighborIndex( index[0], index[1]));
+            for (Integer[] neighbor : Ln) {
+                if (this.neighborhoodAboveThresh(neighbor[0], neighbor[1], tmin)&& this.neighborhoodBellowThresh(neighbor[0], neighbor[1], tmax)) L.add(neighbor);
+            }
+        }
+        ;
+        return L;
+    }
+
+    public ArrayList<Integer[]> ListWillDead( ArrayList<Integer[]> Lalive, int tmin, int tmax) {
+
+        ArrayList<Integer[]> L = new ArrayList<Integer[]>();
+        ArrayList<Integer[]> Ln;//list empty neighborhood
+
+        for (Integer[] index : Lalive) {
+
+            if (this.neighborhoodAboveThresh(index[0], index[1], tmax)) L.add(index);
+            if (this.neighborhoodBellowThresh(index[0], index[1], tmin)) L.add(index);
+        }
+        return L;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
